dfd2686aa654df3893a265c8286831f112ec1f0f
scraptechguy <rostislav.bro@gmail.com>
2021-12-18 00:23:19 +0100

    Initial Commit

---
+++ StuduWidgets/StuduWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuduWidgetsApp.swift
+//  StuduWidgets
+//
+//  Created by Rostislav Brož on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct StuduWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
