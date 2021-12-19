d16c744051821892e26967e09f63b7e28495cb90
scraptechguy <rostislav.bro@gmail.com>
2021-12-19 19:53:20 +0100

    Added a declaration of screen size constant and UI colors in ContentView.swift

--- StuduWidgets/ContentView.swift
+++ StuduWidgets/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+
+// declaration of a constant from which screen width and height is accessed to properly position and size objects
+
+let screenSize: CGRect = UIScreen.main.bounds
+
+
+// declaration of UI colors
+
+public var bg = Color(red: 0.1333, green: 0.1333, blue: 0.1373)
+public var fontClr = Color(red: 1, green: 1, blue: 1)
+
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            bg.ignoresSafeArea()
+            
+            VStack {
+                Text("Widgets")
+                    .foregroundColor(fontClr)
+            }
+        }
     }
 }
 
