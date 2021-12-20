773d23209799d6b99f08672080cd54ec041e1950
scraptechguy <rostislav.bro@gmail.com>
2021-12-20 10:08:35 +0100

    Create a navigation pane at the bottom of the UI

--- StuduWidgets/Home/ContentView.swift
+++ StuduWidgets/Home/ContentView.swift
@@ -59,6 +59,13 @@ struct HomeView: View {
                         }).sheet(isPresented: $showingTimetableWidgetSheet) {TimetableSheet()}
                     }
                 }
+                
+                ZStack {
+                    Rectangle()
+                        .fill(objectsClr)
+                        .frame(width: screenSize.width, height: screenSize.width / 5)
+                        .ignoresSafeArea()
+                }
             }
         }
     }
