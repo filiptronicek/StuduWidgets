bdaedce633712f7a9f03c02b86687f2920e01ce3
Rostislav Brož <rostislav.broz@outlook.com>
2021-12-27 18:25:47 +0100

    Wrap ContentModel code to a ContentModel class that's accessible throughout the app

--- StuduWidgets/ViewModels/ContentModel.swift
+++ StuduWidgets/ViewModels/ContentModel.swift
@@ -7,40 +7,38 @@
 
 import Foundation
 
-func fetchAPI<D: Decodable>(url: URL) async throws -> D {
-    let task = Task { () -> D in
-        let data = try Data(contentsOf: url)
-        let decodedData = try JSONDecoder().decode(D.self, from: data)
-        return decodedData
-    }
-    return try await task.value
-}
-
-let endpoint = "https://strava-api.vercel.app/api"
-
-// TODO - linking to the API
-func getStravaToken (username: String, password: String, canteen: String) async -> String? {
-    
-    struct Result: Decodable {
-        var token: String
-    }
-    
-    struct Response: Decodable {
-       var status: String
-       var result: Result
-    }
-    
-    do {
-        let url = URL(string: "\(endpoint)/auth/token?username=\(username)&password=\(password)&canteen=\(canteen)")
-        guard let requestUrl = url else { fatalError() }
-        let data: Response = try await fetchAPI(url: requestUrl)
-        return data.result.token
-    } catch {
-        print(error.localizedDescription)
-        return nil
-    }
-}
-    
 class ContentModel: ObservableObject {
-    var timetable = "m"
+    func fetchAPI<D: Decodable>(url: URL) async throws -> D {
+        let task = Task { () -> D in
+            let data = try Data(contentsOf: url)
+            let decodedData = try JSONDecoder().decode(D.self, from: data)
+            return decodedData
+        }
+        return try await task.value
+    }
+
+    let endpoint = "https://strava-api.vercel.app/api"
+
+    // TODO - linking to the API
+    func getStravaToken (username: String, password: String, canteen: String) async -> String? {
+        
+        struct Result: Decodable {
+            var token: String
+        }
+        
+        struct Response: Decodable {
+           var status: String
+           var result: Result
+        }
+        
+        do {
+            let url = URL(string: "\(endpoint)/auth/token?username=\(username)&password=\(password)&canteen=\(canteen)")
+            guard let requestUrl = url else { fatalError() }
+            let data: Response = try await fetchAPI(url: requestUrl)
+            return data.result.token
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 }
