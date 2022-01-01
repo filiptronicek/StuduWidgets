f7d382ce7f550217cd23ac61eab556bd9715fa23
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-01 14:23:34 +0100

    Create confetti effect

--- StuduWidgets/Concepts/ConfettiConcept.swift
+++ StuduWidgets/Concepts/ConfettiConcept.swift
@@ -9,7 +9,72 @@ import SwiftUI
 
 struct ConfettiConcept: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Home()
+    }
+}
+
+struct Home: View {
+    @State var buttonPressed:Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Button(action: {},
+                       label: {
+                            Text("Confetti GO!")
+                                .foregroundColor(Color.black)
+                })
+            }
+            
+            EmitterView()
+                .scaleEffect(buttonPressed ? 1 : 0, anchor: .top)
+                .opacity(!buttonPressed ? 1 : 0)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct EmitterView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .red
+        
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.emitterShape = .line
+        emitterLayer.emitterCells = createEmitterCells()
+        emitterLayer.emitterSize = CGSize(width: screenSize.width, height: 1)
+        emitterLayer.emitterPosition = CGPoint(x: screenSize.width / 2, y: 0)
+        
+        view.layer.addSublayer(emitterLayer)
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    func createEmitterCells() -> [CAEmitterCell] {
+        var emitterCells: [CAEmitterCell] = []
+        
+        for index in 1...12 {
+            let cell = CAEmitterCell()
+            
+            cell.contents = UIImage(named: "Rectangle")?.cgImage
+            cell.color = UIColor.red.cgColor
+            cell.birthRate = 4.5
+            cell.lifetime = 20
+            cell.velocity = 120
+            cell.scale = 0.2
+            cell.emissionLongitude = .pi
+            cell.emissionRange = 0.5
+            cell.spin = 3.5
+            cell.spinRange = 1
+            
+            emitterCells.append(cell)
+        }
+        
+        return emitterCells
     }
 }
 
