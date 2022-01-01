3660819856147193af43c7978131ba25174e014c
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-01 13:35:20 +0100

    Update file management and create ConfettiConcept.swift

---
+++ StuduWidgets/Concepts/ConfettiConcept.swift
@@ -0,0 +1,20 @@
+//
+//  ConfettiConcept.swift
+//  StuduWidgets
+//
+//  Created by Rostislav Brož on 1/1/22.
+//
+
+import SwiftUI
+
+struct ConfettiConcept: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfettiConcept_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfettiConcept()
+    }
+}
