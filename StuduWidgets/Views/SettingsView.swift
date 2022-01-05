bfa09ecacefdbef886d644c265c8058431c25060
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-05 17:36:46 +0100

    Put Widgets and Settings sections into separate files

--- StuduWidgets/Views/SettingsView.swift
+++ StuduWidgets/Views/SettingsView.swift
@@ -8,13 +8,129 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // Access data in ContentModel.swift
+    
+    @EnvironmentObject var model: ContentModel
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                if !model.isSignedInToBakalari && !model.isSignedInToStrava {
+                    Text("Get started")
+                        .foregroundColor(model.objectsClrLight)
+                        .font(.system(size: screenSize.width / 15))
+                        .padding(.trailing, screenSize.width / 1.8)
+                        .padding(.bottom, screenSize.width / 60)
+                    
+                    Text("For a widget to work, you need to **sign in** \nto a corresponding service...")
+                        .foregroundColor(model.fontClr)
+                        .font(.system(size: screenSize.width / 25))
+                        .frame(width: screenSize.width / 1.2, height: screenSize.width / 10)
+                        .padding(.bottom, screenSize.width / 25)
+                        .padding(.trailing, screenSize.width / 9)
+
+                    HStack {
+                        Image(systemName: "gearshape")
+                            .resizable()
+                            .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                            .foregroundColor(model.fontClr)
+                                                            
+                        Text("Open **Settings**")
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: screenSize.width / 25))
+                            .padding(.leading, screenSize.width / 30)
+                        
+                        Spacer()
+                    }.padding(.top, screenSize.width / 40)
+                        .frame(width: screenSize.width / 1.2, height: screenSize.width / 15)
+                    
+                    HStack {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                            .foregroundColor(model.fontClr)
+                        
+                        Text("Click on **Add account**")
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: screenSize.width / 25))
+                            .padding(.leading, screenSize.width / 30)
+                        
+                        Spacer()
+                    }.padding(.top, screenSize.width / 50)
+                        .frame(width: screenSize.width / 1.2, height: screenSize.width / 15)
+                    
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                            .foregroundColor(model.fontClr)
+                        
+                        Text("**Sign in** to a service")
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: screenSize.width / 25))
+                            .padding(.leading, screenSize.width / 30)
+                        
+                        Spacer()
+                    }.padding(.top, screenSize.width / 14)
+                        .padding(.bottom, screenSize.width / 18)
+                        .frame(width: screenSize.width / 1.2, height: screenSize.width / 15)
+                }
+                
+                Text("Widgets")
+                    .foregroundColor(model.objectsClrLight)
+                    .font(.system(size: screenSize.width / 15))
+                    .padding(.trailing, screenSize.width / 1.6)
+                    .padding(.top, screenSize.width / 18)
+                
+                Button(action: {model.showingTimetableSheet.toggle()}, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(model.objectsClrDark)
+                                .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                .cornerRadius(15)
+                            
+                            HStack {
+                                Text("Timetable Widget")
+                                    .foregroundColor(model.fontClr)
+                                    .font(.system(size: screenSize.width / 16))
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .frame(width: screenSize.width / 30, height: screenSize.width / 20)
+                                    .padding(.leading, screenSize.width / 5.95)
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
+                }).sheet(isPresented: $model.showingTimetableSheet) {TimetableSheet()}
+                
+                Button(action: {model.showingLunchSheet.toggle()},
+                       label: {
+                            ZStack {
+                                Rectangle()
+                                    .fill(model.objectsClrDark)
+                                    .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                    .cornerRadius(15)
+                                
+                                HStack {
+                                    Text("Lunch Widget")
+                                        .foregroundColor(model.fontClr)
+                                        .font(.system(size: screenSize.width / 16))
+                                    Image(systemName: "chevron.right")
+                                        .resizable()
+                                        .frame(width: screenSize.width / 30, height: screenSize.width / 20)
+                                        .padding(.leading, screenSize.width / 3.75)
+                                        .foregroundColor(Color.gray)
+                                }
+                            }
+                }).sheet(isPresented: $model.showingLunchSheet) {LunchSheet()}
+            }
+        }
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+            .environmentObject(ContentModel())
     }
 }
--- StuduWidgets/Views/WidgetsView.swift
+++ StuduWidgets/Views/WidgetsView.swift
@@ -8,8 +8,227 @@
 import SwiftUI
 
 struct WidgetsView: View {
+    // Access data in ContentModel.swift
+    
+    @EnvironmentObject var model: ContentModel
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                VStack {
+                    Text("Accounts")
+                        .foregroundColor(model.objectsClrLight)
+                        .font(.system(size: screenSize.width / 15))
+                        .padding(.trailing, screenSize.width / 1.8)
+                        // this padding is 0.3 pixels off and it's pissing me off
+                    
+                    VStack {
+                        if model.isSignedInToBakalari {
+                            ZStack {
+                                Rectangle()
+                                    .fill(model.objectsClrDark)
+                                    .frame(width: screenSize.width / 1.1, height: screenSize.width / 4)
+                                    .cornerRadius(15)
+                                
+                                HStack {
+                                    Spacer()
+                                    
+                                    Image("frightened")
+                                        .resizable()
+                                        .frame(width: screenSize.width / 5.5, height: screenSize.width / 5.5)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(model.fontClr, lineWidth: screenSize.width / 150))
+                                        .padding(.trailing, screenSize.width / 50)
+                                    
+                                    VStack {
+                                        Text("Rostislav Brož")
+                                            .foregroundColor(model.fontClr)
+                                            .font(.system(size: screenSize.width / 18))
+                                        
+                                        Text("Bakalari")
+                                            .foregroundColor(model.objectsClrLight)
+                                            .font(.system(size: screenSize.width / 25))
+                                    }
+                                    
+                                    Spacer()
+                                }
+                            }.contextMenu {
+                                Button(role: .destructive,
+                                       action: {
+                                            model.isSignedInToBakalari = false
+                                        },
+                                       
+                                       label: {
+                                            Label("Sign out", systemImage: "trash")
+                                })
+                            }
+                        }
+                            
+                        if model.isSignedInToStrava {
+                                ZStack {
+                                    Rectangle()
+                                        .fill(model.objectsClrDark)
+                                        .frame(width: screenSize.width / 1.1, height: screenSize.width / 4)
+                                        .cornerRadius(15)
+                                    
+                                    HStack {
+                                        Spacer()
+                                        
+                                        Image("frightened")
+                                            .resizable()
+                                            .frame(width: screenSize.width / 5.5, height: screenSize.width / 5.5)
+                                            .clipShape(Circle())
+                                            .overlay(Circle().stroke(model.fontClr, lineWidth: screenSize.width / 150))
+                                            .padding(.trailing, screenSize.width / 50)
+                                        
+                                        VStack {
+                                            Text("Rostislav Brož")
+                                                .foregroundColor(model.fontClr)
+                                                .font(.system(size: screenSize.width / 18))
+                                            
+                                            Text("Strava")
+                                                .foregroundColor(model.objectsClrLight)
+                                                .font(.system(size: screenSize.width / 25))
+                                        }
+                                        
+                                        Spacer()
+                                    }
+                                }.contextMenu {
+                                    Button(role: .destructive,
+                                           action: {
+                                                model.isSignedInToStrava = false
+                                            },
+                                           
+                                           label: {
+                                                Label("Sign out", systemImage: "trash")
+                                    })
+                                }
+                            }
+                            
+                            Menu {
+                                if !model.isSignedInToBakalari {
+                                    Button (action: {
+                                                // Todo(ft): add sign in action
+                                                model.isSignedInToBakalari = true
+                                            },
+                                            
+                                            label: {
+                                                Label("Sign in to Bakaláři.cz", systemImage: "graduationcap")
+                                    })
+                                }
+                                
+                                if !model.isSignedInToStrava {
+                                    Button (action: {
+                                                // Todo(ft): add sign in action
+                                                model.isSignedInToStrava = true
+                                            },
+                                            label: {
+                                                Label("Sign in to Strava.cz", systemImage: "fork.knife")
+                                    })
+                                }
+                            } label: {
+                                    ZStack {
+                                        if !(model.isSignedInToStrava && model.isSignedInToBakalari) {
+
+                                        Rectangle()
+                                                .fill(model.objectsClrDark)
+                                            .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                            .cornerRadius(15)
+                                        
+                                        HStack {
+                                            Image(systemName: "plus")
+                                                .resizable()
+                                                .foregroundColor(Color.blue)
+                                                .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                                            
+                                            Text("Add account")
+                                                .foregroundColor(Color.blue)
+                                                .font(.system(size: screenSize.width / 18))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    Text("Color theme")
+                        .foregroundColor(model.objectsClrLight)
+                        .font(.system(size: screenSize.width / 15))
+                        .padding(.top, screenSize.width / 15)
+                        .padding(.trailing, screenSize.width / 1.986)
+                    
+                    Button(action: {model.changeColorTheme()}, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(model.objectsClrDark)
+                                .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                .cornerRadius(15)
+                            HStack {
+                                Text("Dark (default)")
+                                    .foregroundColor(model.fontClr)
+                                    .font(.system(size: screenSize.width / 18))
+                                    .frame(width: screenSize.width / 1.5, height: screenSize.width / 8, alignment: .leading)
+                                    
+                                
+                                Image(systemName: model.isDarkmode ? "checkmark" : "")
+                                    .resizable()
+                                    .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                                    .foregroundColor(model.objectsClrLight)
+                                    
+                            }
+                        }
+                    })
+                    
+                    Button(action: {model.changeColorTheme()}, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(model.objectsClrDark)
+                                .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                .cornerRadius(15)
+                            HStack {
+                                Text("Light (lamer)")
+                                    .foregroundColor(model.fontClr)
+                                    .font(.system(size: screenSize.width / 18))
+                                    .frame(width: screenSize.width / 1.5, height: screenSize.width / 8, alignment: .leading)
+                                    
+                                
+                                Image(systemName: model.isDarkmode ? "" : "checkmark")
+                                    .resizable()
+                                    .frame(width: screenSize.width / 20, height: screenSize.width / 20)
+                                    .foregroundColor(model.objectsClrLight)
+                                    
+                            }
+                        }
+                    })
+                    
+                    Text("Information")
+                        .foregroundColor(model.objectsClrLight)
+                        .font(.system(size: screenSize.width / 15))
+                        .padding(.top, screenSize.width / 15)
+                        .padding(.trailing, screenSize.width / 1.986)
+                    
+                    Button(action: {model.showingDataSheet.toggle()}, label: {
+                            ZStack {
+                                Rectangle()
+                                    .fill(model.objectsClrDark)
+                                    .frame(width: screenSize.width / 1.1, height: screenSize.width / 8)
+                                    .cornerRadius(15)
+                                
+                                HStack {
+                                    Text("Data handling")
+                                        .foregroundColor(model.fontClr)
+                                        .font(.system(size: screenSize.width / 16))
+                                    Image(systemName: "chevron.right")
+                                        .resizable()
+                                        .frame(width: screenSize.width / 30, height: screenSize.width / 20)
+                                        .padding(.leading, screenSize.width / 3.68)
+                                        .foregroundColor(Color.gray)
+                                }
+                            }
+                    }).sheet(isPresented: $model.showingDataSheet) {DataSheet()}
+                }
+            }
+        }
     }
 }
 
