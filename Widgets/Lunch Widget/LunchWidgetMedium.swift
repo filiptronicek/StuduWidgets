adeb1a5f1bf8f3b915536342f00f3b1e6d8bed72
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-06 12:58:43 +0100

    Create a new group for Lunch Widget

---
+++ Widgets/Lunch Widget/LunchWidgetMedium.swift
@@ -0,0 +1,20 @@
+//
+//  LunchWidgetMedium.swift
+//  WidgetsExtension
+//
+//  Created by Rostislav Brož on 1/6/22.
+//
+
+import SwiftUI
+
+struct LunchWidgetMedium: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LunchWidgetMedium_Previews: PreviewProvider {
+    static var previews: some View {
+        LunchWidgetMedium()
+    }
+}
---
+++ Widgets/Lunch Widget/LunchWidgetSmall.swift
@@ -0,0 +1,20 @@
+//
+//  LunchWidgetSmall.swift
+//  WidgetsExtension
+//
+//  Created by Rostislav Brož on 1/6/22.
+//
+
+import SwiftUI
+
+struct LunchWidgetSmall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LunchWidgetSmall_Previews: PreviewProvider {
+    static var previews: some View {
+        LunchWidgetSmall()
+    }
+}
