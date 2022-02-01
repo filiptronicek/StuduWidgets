488d355994281ccdc5d9a4e055fbadb6ec86722c
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-01 09:35:14 +0100

    Update formatting in `LabelStyles.swift`

--- StuduWidgets/LabelStyles.swift
+++ StuduWidgets/LabelStyles.swift
@@ -8,38 +8,6 @@
 import SwiftUI
 
 
-// MARK: Button label
-
-struct ButtonLabel: View {
-    // Access data in ContentModel.swift
-    
-    @EnvironmentObject var model: ContentModel
-    
-    var text: String
-    
-    
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(model.objectsClrDark)
-                .frame(width: model.screenSize.width / 1.1, height: model.screenSize.width / 8)
-                .cornerRadius(model.screenSize.width / 28)
-        
-            Text(text)
-                .foregroundColor(model.fontClr)
-                .font(.system(size: model.screenSize.width / 17))
-                .frame(width: model.screenSize.width / 1.4, height: model.screenSize.width / 8, alignment: .leading)
-            
-            Image(systemName: "chevron.right")
-                .resizable()
-                .frame(width: model.screenSize.width / 30, height: model.screenSize.width / 20)
-                .padding(.leading, model.screenSize.width / 1.5)
-                .foregroundColor(.gray)
-        }
-    }
-}
-
-
 // MARK: Heading
 
 struct Heading: View {
@@ -74,6 +42,38 @@ struct Title: View {
             .frame(width: model.screenSize.width / 1.15, alignment: .leading)
             .padding([.bottom], model.screenSize.width / 18)
             .padding([.top], model.screenSize.width / 16)
+    }
+}
+
+
+// MARK: Button label
+
+struct ButtonLabel: View {
+    // Access data in ContentModel.swift
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var text: String
+    
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(model.objectsClrDark)
+                .frame(width: model.screenSize.width / 1.1, height: model.screenSize.width / 8)
+                .cornerRadius(model.screenSize.width / 28)
+        
+            Text(text)
+                .foregroundColor(model.fontClr)
+                .font(.system(size: model.screenSize.width / 17))
+                .frame(width: model.screenSize.width / 1.4, height: model.screenSize.width / 8, alignment: .leading)
+            
+            Image(systemName: "chevron.right")
+                .resizable()
+                .frame(width: model.screenSize.width / 30, height: model.screenSize.width / 20)
+                .padding(.leading, model.screenSize.width / 1.5)
+                .foregroundColor(.gray)
+        }
     }
 }
 
