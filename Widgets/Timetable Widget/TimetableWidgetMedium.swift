739f7de94cc4febd2b847ddf4705683a32da0c6a
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-06 21:21:56 +0100

    Implement initial design of the medium sized TimetableWidget

--- Widgets/Timetable Widget/TimetableWidgetMedium.swift
+++ Widgets/Timetable Widget/TimetableWidgetMedium.swift
@@ -16,6 +16,77 @@ struct TimetableWidgetMedium: View {
     var entry: SimpleEntry
     
     var body: some View {
-        Text("TimetableWidgetMedium")
+        ZStack {
+            model.bg
+            
+            Rectangle()
+                .fill(model.objectClrBlue)
+                .frame(width: model.screenSize.width / 300, height: model.screenSize.width)
+            
+            VStack {
+                HStack {
+                    ZStack {
+                        Text(model.timetableTuesday[0])
+                            .foregroundColor(model.fontClrDark)
+                            .font(.system(size: model.screenSize.width / 8))
+                            .padding([.top, .leading], model.screenSize.width / 60)
+                        
+                        Text(model.timetableTuesday[0])
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: model.screenSize.width / 8))
+                    }
+                    
+                    Rectangle()
+                        .fill(model.fontClr)
+                        .frame(width: model.screenSize.width / 150, height: model.screenSize.width / 10)
+                        .padding([.vertical], model.screenSize.width / 8)
+                    
+                    ZStack {
+                        Text(model.timetableTuesday[1])
+                            .foregroundColor(model.fontClrDark)
+                            .font(.system(size: model.screenSize.width / 8))
+                            .padding([.top, .leading], model.screenSize.width / 60)
+                        
+                        Text(model.timetableTuesday[1])
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: model.screenSize.width / 8))
+                    }
+                    
+                    Rectangle()
+                        .fill(model.fontClr)
+                        .frame(width: model.screenSize.width / 150, height: model.screenSize.width / 10)
+                    
+                    ZStack {
+                        Text(model.timetableTuesday[2])
+                            .foregroundColor(model.fontClrDark)
+                            .font(.system(size: model.screenSize.width / 8))
+                            .padding([.top, .leading], model.screenSize.width / 60)
+                        
+                        Text(model.timetableTuesday[2])
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: model.screenSize.width / 8))
+                    }
+                    
+                    Rectangle()
+                        .fill(model.fontClr)
+                        .frame(width: model.screenSize.width / 150, height: model.screenSize.width / 10)
+                    
+                    ZStack {
+                        Text(model.timetableTuesday[3])
+                            .foregroundColor(model.fontClrDark)
+                            .font(.system(size: model.screenSize.width / 8))
+                            .padding([.top, .leading], model.screenSize.width / 60)
+                        
+                        Text(model.timetableTuesday[3])
+                            .foregroundColor(model.fontClr)
+                            .font(.system(size: model.screenSize.width / 8))
+                    }
+                    
+                    // TODO - Create child appending based on the number of subjects in a list
+                    
+                }.frame(width: model.screenSize.width / 0.5, height: model.screenSize.width)
+                    .offset(x: 100)
+            }
+        }
     }
 }
