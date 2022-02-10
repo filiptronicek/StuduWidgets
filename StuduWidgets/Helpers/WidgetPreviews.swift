57e870c26280fada9f0e6d37a11b496d3b3ad243
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-10 15:20:05 +0100

    Update contents of `LunchWidgetSmallPreview` struct

--- StuduWidgets/Helpers/WidgetPreviews.swift
+++ StuduWidgets/Helpers/WidgetPreviews.swift
@@ -48,7 +48,35 @@ struct LunchWidgetSmallPreview: View {
     var mockData: String
     
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .fill(model.WobjectsClrDark)
+                .frame(width: model.screenSize.width / 4.5, height: model.screenSize.width / 4.5)
+                .cornerRadius(15)
+        
+            HStack() {
+                Text(Date.now, format: .dateTime.day())
+                    .foregroundColor(model.WfontClr)
+                    .font(.system(size: model.screenSize.width / 10))
+                
+                Text(Date.now, format: .dateTime.weekday())
+                    .foregroundColor(model.WfontClr)
+                    .font(.system(size: model.screenSize.width / 20))
+            }.padding([.bottom], model.screenSize.width / 5.5)
+            
+            VStack {
+                Text(mockData)
+                    .foregroundColor(model.WfontClr)
+                    .font(.system(size: model.screenSize.width / 28))
+                    .frame(width: model.screenSize.width / 3.2, height: model.screenSize.width / 6)
+                    .padding([.horizontal], model.screenSize.width / 40)
+                    .background(
+                        Rectangle()
+                            .fill(model.WobjectsClrMedium)
+                            .cornerRadius(model.screenSize.width / 28)
+                    )
+            }.padding([.top], model.screenSize.width / 6.3)
+        }
     }
 }
 
