17519a04c1520dcd424a4f6e206ae45126184c7f
Rostislav Brož <rostislav.broz@outlook.com>
2022-03-12 11:19:12 +0100

    Create separate file for `NavigationMenu`

--- StuduWidgets/Views/Home/HomeView.swift
+++ StuduWidgets/Views/Home/HomeView.swift
@@ -45,73 +45,7 @@ struct HomeView: View {
                 
             // MARK: Navigation menu
             
-            VStack {
-                Spacer()
-                
-                HStack {
-                    Spacer()
-                    
-                    Button(action: {
-                                withAnimation {
-                                    model.showingSettings = 0
-                                }
-                            },
-                           label: {
-                                VStack {
-                                    if model.showingSettings == 0 {
-                                        Image(systemName: "square.text.square.fill")
-                                            .resizable()
-                                            .frame(width: model.screenSize.width / 18, height: model.screenSize.width / 18)
-                                            .foregroundColor(model.fontClr)
-                                    } else {
-                                        Image(systemName: "square.text.square")
-                                            .resizable()
-                                            .frame(width: model.screenSize.width / 18, height: model.screenSize.width / 18)
-                                            .foregroundColor(model.fontClr)
-                                    }
-                                    
-                                    Text("Widgets")
-                                        .foregroundColor(model.fontClr)
-                                        .font(.system(size: model.screenSize.width / 30))
-                                }
-                    })
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                                withAnimation {
-                                    model.showingSettings = 1
-                                }
-                            },
-                           label: {
-                                VStack {
-                                    if model.showingSettings == 1 {
-                                        Image(systemName: "gearshape.fill")
-                                            .resizable()
-                                            .frame(width: model.screenSize.width / 18, height: model.screenSize.width / 18)
-                                            .foregroundColor(model.fontClr)
-                                    } else {
-                                        Image(systemName: "gearshape")
-                                            .resizable()
-                                            .frame(width: model.screenSize.width / 18, height: model.screenSize.width / 18)
-                                            .foregroundColor(model.fontClr)
-                                    }
-                                    
-                                    Text("Settings")
-                                        .foregroundColor(model.fontClr)
-                                        .font(.system(size: model.screenSize.width / 30))
-                                }
-                    })
-                    
-                    Spacer()
-                }.padding([.bottom], model.screenSize.width / 11)
-                    .background(
-                        Rectangle()
-                            .fill(LinearGradient(gradient: Gradient(stops: [.init(color: model.bg.opacity(0.01), location: 0), .init(color: model.bg, location: 1)]), startPoint: .top, endPoint: .bottom))
-                            .frame(width: model.screenSize.width, height: model.screenSize.width / 3)
-                            .padding([.bottom], model.screenSize.width / 12)
-                    )
-            }.ignoresSafeArea()
+            NavigationMenu()
         }.preferredColorScheme(.dark)
     }
 }
